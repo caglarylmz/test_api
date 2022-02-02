daf5a5d1610e2c5c63f540a935b638760d9b2fbf
caglarylmz <caglar.ylmz@outlook.com>
2022-02-02 22:43:38 +0300

    fix heroku error

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 const routes = require("./routes/index");
+require("dotenv").config();
 
 connectDB();
 
--- src/config/passport.js
+++ src/config/passport.js
@@ -1,12 +1,13 @@
 var JwtStrategy = require("passport-jwt").Strategy;
 var ExtractJwt = require("passport-jwt").ExtractJwt;
 require("dotenv").config();
-
+var dbconfig = require("./dbconfig");
 var User = require("../models/user");
 
 module.exports = (passport) => {
   var opts = {};
-
+  console.log(dbconfig.secret);
+  console.log(process.env.SECRET_KEY);
   opts.secretOrKey = process.env.SECRET_KEY;
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
   passport.use(
